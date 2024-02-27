b7b2dd13f43ed8db233ff499e72ef02a64e8eb6d
DanielR2132 <154104609+DanielR2132@users.noreply.github.com>
2024-02-26 18:36:27 -0600

    3

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -7,6 +7,33 @@ describe("Command class", function() {
 
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
-  });
+});
+});
 
+describe('Command class', function() {
+  describe('constructor', function() {
+    it('sets command type', function() {
+      const expectedType = 'testCommand';
+      const command = new Command(expectedType);
+      expect(command.commandType).toBe(expectedType);
+    });
+
+    it('throws error for missing command type', function() {
+      try {
+        new Command();
+      } catch (error) {
+        expect(error.message).toBe('Command type required.');
+      }
+});
+});
+});
+
+describe('Command class', function() {
+  describe('constructor', function() {
+    it('sets value', function() {
+      const expectedValue = 'some random value';
+      const command = new Command('testCommand', expectedValue);
+      expect(command.value).toBe(expectedValue);
+});
+});
 });
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,7 +7,102 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
-
-  // 7 tests here!
-
+  it('sets position and default values for mode and generatorWatts', function() {
+    const rover = new Rover(Number);
+    expect(rover.position).toBe(Number);
+    expect(rover.mode).toBe('NORMAL');
+    expect(rover.generatorWatts).toBe(110);
 });
+});
+
+describe('recieveMessage', function() {
+  it('returns a response containing the message name', function() {
+    const rover = new Rover();
+    const messageName = 'Test Message';
+    const commands = [new Command('MOVE', 10)];
+    const message = new Message(messageName, commands);
+
+    const response = rover.recieveMessage(message);
+
+    expect(response.message).toBe(messageName);
+});
+});
+
+ describe('recieveMessage', function() {
+    it('returns a response with two results for two commands', function() {
+      const rover = new Rover();
+      const messageName = 'Test Message';
+      const commands = [
+        new Command('STATUS_CHECK'),
+        new Command('MOVE', 10),
+      ];
+      const message = new Message(messageName, commands);
+      const response = rover.recieveMessage(message);
+      expect(response.results.length).toBe(2);
+  });
+  });  
+ describe('recieveMessage', function() {
+    it('responds correctly to the STATUS_CHECK command', function() {
+      const rover = new Rover(100, 'LOW_POWER', 200);
+      const messageName = 'Status Check';
+      const command = new Command('STATUS_CHECK');
+      const message = new Message(messageName, [command]);
+
+      const response = rover.recieveMessage(message);
+      const expectedStatus = {
+        mode: rover.mode,
+        generatorWatts: rover.generatorWatts,
+        position: rover.position,
+      };
+      expect(response.results[0].completed).toBe(true);
+      expect(response.results[0].roverStatus).toEqual(expectedStatus);
+  });
+  });
+
+  describe('recieveMessage', function() {
+    it('responds correctly to the MODE_CHANGE command', function() {
+      const rover = new Rover(100, 'NORMAL');
+      const lowPowerCommand = new Command('MODE_CHANGE', 'LOW_POWER');
+      const lowPowerMessage = new Message('Mode Change', [lowPowerCommand]);
+      const lowPowerResponse = rover.recieveMessage(lowPowerMessage);
+
+      expect(lowPowerResponse.results[0].completed).toBe(true);
+      expect(rover.mode).toBe('LOW_POWER');
+
+      const normalCommand = new Command('MODE_CHANGE', 'NORMAL');
+      const normalMessage = new Message('Mode Change', [normalCommand]);
+      const normalResponse = rover.recieveMessage(normalMessage);
+
+      expect(normalResponse.results[0].completed).toBe(true);
+      expect(rover.mode).toBe('NORMAL');
+  });
+  });
+
+  describe('recieveMessage', function() {
+    it('responds with a false completed value when attempting to move in LOW_POWER mode', function() {
+      const initialPosition = 100;
+      const rover = new Rover(initialPosition, 'LOW_POWER');
+
+      const moveCommand = new Command('MOVE', 20);
+      const message = new Message('Move', [moveCommand]);
+      const response = rover.recieveMessage(message);
+
+      expect(response.results[0].completed).toBe(false);
+      expect(rover.position).toBe(initialPosition);
+  });
+  });
+
+  describe('recieveMessage', function() {
+    it('responds with the position for the MOVE command', function() {
+      const initialPosition = 100;
+      const rover = new Rover(initialPosition, 'NORMAL');
+
+      const newPosition = 200;
+      const moveCommand = new Command('MOVE', newPosition);
+      const message = new Message('Move', [moveCommand]);
+      const response = rover.recieveMessage(message);
+
+      expect(response.results[0].completed).toBe(true);
+      expect(rover.position).toBe(newPosition);
+  });
+  });
